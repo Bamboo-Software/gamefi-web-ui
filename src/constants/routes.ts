0794efccfeb9d68dd8f7c948ae0d402a66bd63a6
phi.hoang.nguyen <phih5066@gmail.com>
2025-03-30 19:15:56 +0700

    fix: add routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -8,6 +8,8 @@ const routes = {
     SELECT_LANGUAGE: '/select-language',
     REFERRAL: '/referral',
     AUTH:'/auth',
+    AUTH_CALLBACK: '/auth/callback',
+
     GAMES_LOTTERY_SPINNER: '/games/lottery-spinner-game',
     GAMES_LOTTERY_SPINNER_HISTORY: '/games/lottery-spinner/history',
     GAMES_FLAPPY_JFOX: '/games/flappy-jfox-game',
