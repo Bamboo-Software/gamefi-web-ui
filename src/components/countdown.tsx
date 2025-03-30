4720bfde9e64f3e4953bff9f644c5b8d7410bc01
phi.hoang.nguyen <phih5066@gmail.com>
2025-03-30 20:34:46 +0700

    fix: fix build failed

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { motion } from "framer-motion";
